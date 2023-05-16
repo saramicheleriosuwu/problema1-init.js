0cc8d843e8f1da805413e75673e8eb11ddb41b27
saramicheleriosuwu <128102798+saramicheleriosuwu@users.noreply.github.com>
2023-05-16 15:08:07 -0300

    Update problem1.js

--- problem1.js
+++ problem1.js
@@ -1,1 +1,11 @@
 
+function multipleOfThree(num) {
+  if (num % 3 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+(multipleOfThree(21)); // true
+(multipleOfThree(10)); // false
+(multipleOfThree(33)); // true
