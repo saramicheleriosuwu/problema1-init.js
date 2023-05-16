979686b3e5a2cc3af9066a88688241b06356b6e5
saramicheleriosuwu <128102798+saramicheleriosuwu@users.noreply.github.com>
2023-05-16 15:02:45 -0300

    Update problem1.js

--- problem1.js
+++ problem1.js
@@ -1,9 +1,1 @@
 
-function humanizeDate() {
-  const date = new Date("2022-12-18");
-  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-  const humanizedDate = date.toLocaleDateString('es-AR', options);
-  return humanizedDate;
-}
-
-console.log(humanizeDate()); // "Domingo, 18 de diciembre de 2022"
